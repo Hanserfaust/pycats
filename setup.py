c64622af2df679e1b535b970033e7b4173e76e05
Hanserfaust <hans.eklund@igw.se>
2013-03-07 09:18:35 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Pycats',
+    version='0.2',
+    description='Time series storage for cassandra, built on Pycassa',
+    author='Hans Eklund',
+    author_email='hans.eklund@igw.se',
+    url='https://github.com/Hanserfaust/pycats',
+    packages=['pycats'],
+)
